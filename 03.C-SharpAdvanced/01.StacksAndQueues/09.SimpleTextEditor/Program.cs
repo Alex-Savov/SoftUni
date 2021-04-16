5aee1fa39c95437b03d3cc534c7b3bfcffe77502
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-04-15 19:05:27 -0500

    added exercises 09, 10

---
+++ 03.C-SharpAdvanced/01.StacksAndQueues/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Stack<string> text = new Stack<string>();
+            text.Push("");
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                string command = input[0];
+
+                switch (command)
+                {
+                    case "1":
+                        string inputText = $"{text.Peek()}{input[1]}";
+                        text.Push(inputText);
+                        break;
+                    case "2":
+                        int count = int.Parse(input[1]);
+                        int startIndex = text.Peek().Length - count;
+                        string substring = text.Peek().Remove(startIndex);
+                        text.Push(substring);
+                        break;
+                    case "3":
+                        int index = (int.Parse(input[1]) - 1 );
+                        Console.WriteLine(text.Peek()[index]);
+                        break;
+                    case "4":
+                        text.Pop();
+                        break;
+                    default:
+                        break;
+                }
+            }          
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/01.StacksAndQueues/10.Crossroads/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+           
+            Queue<string> q = new Queue<string>();
+            int passedCars = 0;
+            bool isThereCrash = false;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int currentGreenLight = greenLight;
+                
+                if (input.ToLower() == "end")
+                {
+                    break;
+                }
+
+                if (input.ToLower() == "green")
+                {
+                    int index = q.Count;
+
+                    for (int i = 0; i < index; i++)
+                    {
+                        string car = q.Peek();
+
+                        if (currentGreenLight == 0)
+                        {
+                            break;
+                        }
+
+                        if (currentGreenLight >= car.Length)
+                        {
+                            currentGreenLight -= car.Length;
+                        }
+                        else
+                        {
+                            string substring = car.Substring(0, currentGreenLight);
+                            car = car.Replace(substring, "");
+
+                            if (car.Length > freeWindow)
+                            {
+                                isThereCrash = true;
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{q.Peek()} was hit at {car[freeWindow]}.");
+                                break;
+                            }
+
+                            currentGreenLight = 0;
+                        }
+
+                        q.Dequeue();
+                        passedCars++;
+                    }
+                }
+                else
+                {
+                    q.Enqueue(input);
+                }
+
+                if (isThereCrash)
+                {
+                    break;
+                }
+            }
+
+            if (!isThereCrash)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{passedCars} total cars passed the crossroads.");
+            }
+        }
+    }
+}
