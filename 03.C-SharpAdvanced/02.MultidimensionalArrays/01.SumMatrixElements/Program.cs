7db7e3ea45026d8be69827b1296e47d143e1a720
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-04-26 10:56:12 -0500

    exercises added

---
+++ 03.C-SharpAdvanced/02.MultidimensionalArrays/01.SumMatrixElements/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = ReadMatrix(matrixSize[0], matrixSize[1]);
+            int matrixSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrixSum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrixSize[0]);
+            Console.WriteLine(matrixSize[1]);
+            Console.WriteLine(matrixSum);
+        }
+
+        static int[,] ReadMatrix(int rowMax, int colMax)
+        {
+            
+            int[,] matrix = new int[rowMax, colMax];
+
+            for (int row = 0; row < rowMax; row++)
+            {
+                int[] matrixElements = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < colMax; col++)
+                {
+                    matrix[row, col] = matrixElements[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/02.MultidimensionalArrays/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = ReadMatrix(matrixSize[0], matrixSize[1]);
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int colSum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    colSum += matrix[row, col];
+                }
+
+                Console.WriteLine(colSum);
+            }
+        }
+        static int[,] ReadMatrix(int rowMax, int colMax)
+        {
+
+            int[,] matrix = new int[rowMax, colMax];
+
+            for (int row = 0; row < rowMax; row++)
+            {
+                int[] matrixElements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < colMax; col++)
+                {
+                    matrix[row, col] = matrixElements[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
