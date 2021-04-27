f8a743c156ae9183a4e7f0c3702f42709cf1bb91
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-04-27 17:12:10 -0500

    exercises added

---
+++ 03.C-SharpAdvanced/02.MultidimensionalArrays/01.DiagonalDifference/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = ReadMatrix(new int[n, n]);
+            int primeDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        primeDiagonalSum += matrix[row, col];
+                    }
+
+                    if (row == (n - 1 - col))
+                    {
+                        secondaryDiagonalSum += matrix[row, col];
+                    }
+                }
+            }
+
+            int difference = Math.Abs(primeDiagonalSum - secondaryDiagonalSum);
+
+            Console.WriteLine(difference);
+        }
+
+        static int[,] ReadMatrix(int[,] matrix)
+        {
+            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/02.MultidimensionalArrays/02.2X2SquaresInMatrix/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._2X2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxRows = dimensions[0];
+            int maxCols = dimensions[1];
+
+            string[,] matrix = ReadMatrix(new string[maxRows, maxCols]);
+            int equalMatrxCount = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    bool isEqual = (matrix[row, col] == matrix[row, col + 1])
+                        && (matrix[row + 1, col] == matrix[row + 1, col + 1])
+                        && (matrix[row, col] == matrix[row + 1, col + 1]);
+
+                    if (isEqual)
+                    {
+                        equalMatrxCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(equalMatrxCount);
+        } 
+        static string[,] ReadMatrix(string[,] matrix)
+        {
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
