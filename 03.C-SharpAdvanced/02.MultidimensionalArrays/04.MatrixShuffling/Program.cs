bf72e3b1c8b26ad2e54d8c7eb304bacea09993c0
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-05-01 11:25:45 -0500

    exercises added

---
+++ 03.C-SharpAdvanced/02.MultidimensionalArrays/04.MatrixShuffling/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            int maxRows = dimensions[0];
+            int maxCols = dimensions[1];
+
+            string[,] matrix = new string[maxRows, maxCols];
+            ReadMatrix(matrix);
+
+            while (true)
+            {
+                string inputCommand = Console.ReadLine();
+
+                if (inputCommand == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = inputCommand
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (IsCommandValid(maxRows, maxCols, tokens))
+                {
+                    int rowFirst = int.Parse(tokens[1]);
+                    int colFirst = int.Parse(tokens[2]);
+                    int rowSecond = int.Parse(tokens[3]);
+                    int colSecond = int.Parse(tokens[4]);
+
+                    string temp = matrix[rowFirst, colFirst];
+                    matrix[rowFirst, colFirst] = matrix[rowSecond, colSecond];
+                    matrix[rowSecond, colSecond] = temp;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                PrinMatrix(matrix);
+            }
+        }
+
+        private static bool IsCommandValid(int maxRows, int maxCols, string[] tokens)
+        {
+            if (tokens.Length == 5 && tokens[0] == "swap")
+            {
+                int rowFirst = int.Parse(tokens[1]);
+                int colFirst = int.Parse(tokens[2]);
+                int rowSecond = int.Parse(tokens[3]);
+                int colSecond = int.Parse(tokens[4]);
+
+                if ((rowFirst >= 0 && rowFirst < maxRows)
+                   && (colFirst >= 0 && colFirst < maxCols)
+                   && (rowSecond >= 0 && rowSecond < maxRows)
+                   && (colSecond >= 0 && colSecond < maxCols))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void ReadMatrix(string[,] matrix)
+        {
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+
+        static void PrinMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/02.MultidimensionalArrays/05.SnakeMoves/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxRows = dimensions[0];
+            int maxCols = dimensions[1];
+
+            char[,] snakeMtx = new char[maxRows, maxCols];
+            int elementCounter = 0;
+            string snake = Console.ReadLine();
+            
+
+            for (int row = 0; row < maxRows; row++)
+            {
+                for (int col = 0; col < maxCols; col++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        snakeMtx[row, col] = snake[elementCounter];
+                    }
+                    else
+                    {
+                        snakeMtx[row, maxCols - 1 - col] = snake[elementCounter];
+                    }
+
+                    elementCounter++;
+
+                    if (elementCounter == snake.Length)
+                    {
+                        elementCounter = 0;
+                    }
+                }
+            }
+
+            PrinMatrix(snakeMtx);
+        }
+        static void PrinMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
