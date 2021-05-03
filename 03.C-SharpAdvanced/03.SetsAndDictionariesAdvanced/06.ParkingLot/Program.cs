740de34ffbacfaf9d28b79d11af10a552a6dae88
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-05-03 16:16:46 -0500

    exercises added

---
+++ 03.C-SharpAdvanced/03.SetsAndDictionariesAdvanced/06.ParkingLot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> lot = new HashSet<string>();
+
+            while (true)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "END")
+                {
+                    break;
+                }
+
+                string command = tokens[0];
+                string regNumber = tokens[1];
+
+                switch (command)
+                {
+                    case "IN":
+                        lot.Add(regNumber);
+                        break;
+                    case "OUT":
+                        lot.Remove(regNumber);
+                        break;
+                }
+            }
+
+            if (lot.Count > 0)
+            {
+                foreach (var car in lot)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/03.SetsAndDictionariesAdvanced/07.SoftUniParty/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guestList = new HashSet<string>();
+
+            string inputList = Console.ReadLine();
+
+            while (inputList != "PARTY")
+            {
+                guestList.Add(inputList);
+                inputList = Console.ReadLine();
+            }
+
+            string guest = Console.ReadLine();
+
+            while (guest != "END")
+            {
+
+                if (guestList.Count == 0)
+                {
+                    break;
+                }
+
+                if (guestList.Contains(guest))
+                {
+                    guestList.Remove(guest);
+                }
+
+                guest = Console.ReadLine();
+            }
+
+            Console.WriteLine(guestList.Count);
+
+            if (guestList.Count > 0)
+            {
+                if (guestList.Any(x => char.IsDigit(x.First())))
+                {
+                    foreach (var item in guestList.Where(x => char.IsDigit(x.First())))
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+                foreach (var item in guestList.Where(x => char.IsLetter(x.First())))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
