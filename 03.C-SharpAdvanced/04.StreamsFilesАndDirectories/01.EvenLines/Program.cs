a6f0641fd5976aa9071c603150ddcaa6e53f766c
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-06-10 16:39:53 -0500

    exercises added

---
+++ 03.C-SharpAdvanced/04.StreamsFilesАndDirectories/01.EvenLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] lines = await File.ReadAllLinesAsync("text.txt");
+            Regex rgx = new Regex(@"[-,.!?]");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    string line = string
+                        .Join(" ", lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse());
+                    line = rgx.Replace(line, "@");
+
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/04.StreamsFilesАndDirectories/02.LineNumbersEX/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _0._2.LineNumbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] lines = await File.ReadAllLinesAsync("text.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string letterPattern = @"\w";
+                string punctPatterm = @"[\/’\(\){}\[\]:;”!\?\-_\.,']";
+                MatchCollection lettersMatches = Regex.Matches(lines[i], letterPattern);
+                MatchCollection punctMayches = Regex.Matches(lines[i], punctPatterm);
+                lines[i] = $"Line {i + 1}: {lines[i]} ({lettersMatches.Count})({punctMayches.Count})";
+            }
+
+            await File.WriteAllLinesAsync("../../../output.txt", lines);
+        }
+    }
+}
