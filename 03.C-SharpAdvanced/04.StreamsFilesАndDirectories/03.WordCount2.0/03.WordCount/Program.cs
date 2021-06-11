c08668d8eb443c6c4c781ae325d81ef7c75aebdf
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-06-11 08:46:21 -0500

    exercise added

---
+++ 03.C-SharpAdvanced/04.StreamsFilesАndDirectories/03.WordCount2.0/03.WordCount/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = {'\\','/','’','(',')','{','}','[',']',':',';','”','!', ',','?','-','_','.',' '};
+            string[] words = File.ReadAllLines("./words.txt")
+                .Select(x => x.ToLower())
+                .ToArray();
+            string[] text = File.ReadAllText("./text.txt")
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToLower())
+                .ToArray();
+            Dictionary<string, int> wordsToCount = new Dictionary<string, int>();
+
+            foreach (var keyWord in words)
+            {
+                foreach (var word in text)
+                {
+                    if (word == keyWord)
+                    {
+                        if (!wordsToCount.ContainsKey(word))
+                        {
+                            wordsToCount.Add(word, 0);
+                        }
+                        wordsToCount[word]++;
+                    }
+                }
+            }
+
+            wordsToCount = wordsToCount
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+            using (StreamWriter wrtr = new StreamWriter("../../../output.txt"))
+            {
+                foreach (var item in wordsToCount)
+                {
+                    wrtr.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
+        }
+    }
+}
