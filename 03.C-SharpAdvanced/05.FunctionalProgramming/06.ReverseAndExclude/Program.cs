336fcefcf2e674181ba5e684c35777576027d929
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-08-25 11:54:29 -0500

    Exercise added

---
+++ 03.C-SharpAdvanced/05.FunctionalProgramming/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> divisibleNumber = (num1, num2) => num1 % num2 != 0;
+            string input = Console.ReadLine();
+            int divider = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(' ', input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(number => divisibleNumber(number, divider))
+                .Reverse()));
+        }
+    }
+}
