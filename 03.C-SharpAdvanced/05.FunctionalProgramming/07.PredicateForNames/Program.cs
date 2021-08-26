83a3374e3a5debb8153274db336477709946944c
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-08-26 10:26:30 -0500

    Exercises added

---
+++ 03.C-SharpAdvanced/05.FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Predicate<string> stringLenght = name => name.Length <= n;
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(n => stringLenght(n))
+                .ToList()
+                .ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/05.FunctionalProgramming/09.ListOfPredicates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxN = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int, bool> divideBySequenceOfNumbers = (dividers, divided) =>
+            {
+                foreach (var item in dividers)
+                {
+                    if (divided % item != 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
+
+            //for (int i = 1; i <= maxN; i++)
+            //{
+            //    if (divideBySequenceOfNumbers(dividers, i))
+            //    {
+            //        Console.Write(i);
+            //        if (i != maxN)
+            //        {
+            //            Console.Write(' ');
+            //        }
+            //    }
+            //}
+
+            int[] filteredNumbers = Enumerable.Range(1, maxN)
+                .Where(n => divideBySequenceOfNumbers(dividers, n))
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", filteredNumbers));
+        }
+    }
+}
