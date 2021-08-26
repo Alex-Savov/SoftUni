1fd77f2f7e5f73b3bf692aa4299e99f9bc0c8c91
Alex-Savov <82216242+Alex-Savov@users.noreply.github.com>
2021-08-26 13:38:12 -0500

    Exercises Added

---
+++ 03.C-SharpAdvanced/05.FunctionalProgramming/10.PredicateParty!/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.PredicateParty_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string commandInput = Console.ReadLine();
+
+
+            while (commandInput.ToLower() != "party!")
+            {
+                string[] tokens = commandInput
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+                string criteria = tokens[1];
+                string comparer = tokens[2];
+
+                names = ListManipulator(names, action, criteria, comparer);
+
+                commandInput = Console.ReadLine();
+            }
+
+            if (names.Count > 0)
+            {
+                Console.Write($"{string.Join(", ", names)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+
+        static Func<string, string, bool> FunctionsPool(string criteria)
+        {
+            Func<string, string, bool> startWith = (name, startString) =>
+            name.Substring(0, startString.Length) == startString;
+
+            Func<string, string, bool> endWith = (name, startString) =>
+            name.Substring(name.Length - startString.Length) == startString;
+
+            Func<string, string, bool> isLenghtEquale = (name, lenght) =>
+            name.Length == int.Parse(lenght);
+
+            switch (criteria.ToLower())
+            {
+                case "startswith": return startWith;
+                case "endswith": return endWith;
+                case "length": return isLenghtEquale;
+                default:
+                    return null;
+            }
+        }
+
+        static List<string> ListManipulator(List<string> names, string action, string criteria, string comparer)
+        {
+            List<string> result = new List<string>();
+            
+            switch (action.ToLower())
+            {
+                case "double":
+                    foreach (var name in names)
+                    {
+                        if (FunctionsPool(criteria)(name, comparer))
+                        {
+                            result.Add(name);
+                        }
+                        result.Add(name);
+                    }
+                    break;
+
+                case "remove":
+
+                    foreach (var name in names)
+                    {
+                        if (FunctionsPool(criteria)(name, comparer))
+                        {
+                            continue;
+                        }
+                        result.Add(name);
+                    }
+                    break;
+                    
+                default:
+                    return names;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 03.C-SharpAdvanced/05.FunctionalProgramming/11.TheParty ReservationFilterModule/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.TheParty_ReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList(); ;
+
+            string command = Console.ReadLine();
+            List<string> filters = new List<string>();
+
+            while (command.ToLower() != "print")
+            {
+                int commandEndIndex = command.IndexOf(';');
+                string action = command.Substring(0, command.Length - (command.Length - commandEndIndex));
+                string filter = command.Substring(commandEndIndex + 1);
+
+                if (action.ToLower().Contains("add"))
+                {
+                    if (!filters.Contains(filter))
+                    {
+                        filters.Add(filter);
+                    }
+                }
+                else if (action.ToLower().Contains("remove"))
+                {
+                    if (filters.Contains(filter))
+                    {
+                        filters.Remove(filter);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var filter in filters)
+            {
+                string[] filterTokens = filter
+                    .Split(';', StringSplitOptions.RemoveEmptyEntries);
+                string criteria = filterTokens[0];
+                string comparer = filterTokens[1];
+
+                names = ListManipulator(names, criteria, comparer);
+            }
+
+            Console.WriteLine(string.Join(' ', names));
+        }
+
+        static Func<string, string, bool> FunctionsPool(string criteria)
+        {
+            Func<string, string, bool> startsWith = (name, startString) =>
+            name.Substring(0, startString.Length) == startString;
+
+            Func<string, string, bool> endsWith = (name, endString) =>
+            name.Substring(name.Length - endString.Length) == endString;
+
+            Func<string, string, bool> isLenghtEqual = (name, lenght) =>
+            name.Length == int.Parse(lenght);
+
+            Func<string, string, bool> contains = (name, containsString) =>
+            name.Contains(containsString);
+
+            switch (criteria.ToLower())
+            {
+                case "starts with": return startsWith;
+                case "ends with": return endsWith;
+                case "length": return isLenghtEqual;
+                case "contains": return contains;
+                default: return null;
+
+            }
+        }
+
+        static List<string> ListManipulator(List<string> names, string criteria, string comparer)
+        {
+            List<string> temp = new List<string>();
+
+            foreach (var name in names)
+            {
+                if (!FunctionsPool(criteria)(name, comparer))
+                {
+                    temp.Add(name);
+                }
+            }
+
+            return temp;
+        }
+    }
+}
